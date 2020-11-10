fae5d2be092c780ef39951815fcc98e6f7093a56
JosueNavarroSolis <56096921+JosueNavarroSolis@users.noreply.github.com>
2020-11-10 00:19:33 -0600

    Avance

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -13,13 +13,16 @@ namespace Proyecto1_BD.Controllers
     {
         SqlConnection con = new SqlConnection();
         SqlCommand com = new SqlCommand();
+        SqlCommand comm = new SqlCommand();
         SqlDataReader dr;
+        SqlDataReader admi;
         // GET: User
         [HttpGet]
         public ActionResult Login()
         {
             return View();
         }
+
         void connectionString()
         {
             con.ConnectionString = "Server = tcp:proyecto1-server-bd.database.windows.net,1433; Database = proyecto1-database; User ID = Administrador; Password = Proyecto1; Trusted_Connection = False; Encrypt = True;";
@@ -30,13 +33,25 @@ namespace Proyecto1_BD.Controllers
             connectionString();
             con.Open();
             com.Connection = con;
+            comm.Connection = con;
             com.CommandText = "Select * from [dbo].[User] US where US.UserName = '"+usr.UserName+"'and US.Password = '"+usr.Password+"'";
             dr = com.ExecuteReader();
             if (dr.Read())
             {
-                con.Close();
+                dr.Close();
+                comm.CommandText = "Select * from [dbo].[User] US where US.UserName = '" + usr.UserName + "'and US.IsAdministrator = '" + true + "'";
+                admi = comm.ExecuteReader();
 
-                return View("Create");
+                if (admi.Read())
+                {
+                    con.Close();
+                    return View("Create");
+                }
+                else
+                {
+                    con.Close();
+                    return View("MenuUser");
+                }
             }
             else
             {
---
+++ Models/EmployeeDAL.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto1_BD.Models
+{
+    public class EmployeeDAL : Controller
+    {
+        string connectionString = "Server = tcp:proyecto1-server-bd.database.windows.net,1433; Database = proyecto1-database; User ID = Administrador; Password = Proyecto1; Trusted_Connection = False; Encrypt = True;";
+    
+        public IEnumerable<User> GetAllEmployee()
+        {
+            List<User> empList = new List<User>();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("User", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    User emp = new User();
+                    emp.Id = Convert.ToInt32(dr["Id"].ToString());
+                    emp.PersonId = Convert.ToInt32(dr["PersonId"].ToString());
+                    emp.UserName = dr["Name"].ToString();
+                    emp.Password = dr["Password"].ToString();
+                    emp.IsAdministrator = Convert.ToBoolean(dr["IsAdministrator"].ToString());
+
+                    empList.Add(emp);
+                }
+                con.Close();
+            }
+            return empList;
+        }
+        public void AddEmployee(User emp)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("[SP_Insert_User]", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Id", emp.Id);
+                cmd.Parameters.AddWithValue("@PersonId", emp.PersonId);
+                cmd.Parameters.AddWithValue("@UserName", emp.UserName);
+                cmd.Parameters.AddWithValue("@Password", emp.Password);
+                cmd.Parameters.AddWithValue("@IsAdministrator", emp.IsAdministrator);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        public void UpdateEmployee(User emp)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("[SP_Update_User]", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Id", emp.Id);
+                cmd.Parameters.AddWithValue("@PersonId", emp.PersonId);
+                cmd.Parameters.AddWithValue("@UserName", emp.UserName);
+                cmd.Parameters.AddWithValue("@Password", emp.Password);
+                cmd.Parameters.AddWithValue("@IsAdministrator", emp.IsAdministrator);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        public void DeleteEmployee(int empPersonId)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("[SP_Delete_User]", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                
+                cmd.Parameters.AddWithValue("@PersonId", emp.PersonId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        public IEnumerable<User> GetEmployee()
+        {
+            User emp = new User();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_Get_User_By_Id", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    emp.Id = Convert.ToInt32(dr["Id"].ToString());
+                    emp.PersonId = Convert.ToInt32(dr["PersonId"].ToString());
+                    emp.UserName = dr["Name"].ToString();
+                    emp.Password = dr["Password"].ToString();
+                    emp.IsAdministrator = Convert.ToBoolean(dr["IsAdministrator"].ToString());
+
+                    empList.Add(emp);
+                }
+                con.Close();
+            }
+            return empList;
+        }
+    }
+}
