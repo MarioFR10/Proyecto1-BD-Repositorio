7552fc0acd89082b79098318165a4588cb290f22
Mario Fernandez <mariosmite123@gmail.com>
2020-11-23 17:58:19 -0600

    Prueba de busqueda de Strings

---
+++ Scripts/prueba_busqueda_strings.sql
@@ -0,0 +1,29 @@
+
+--Para insertar datos de prueba
+select * from [dbo].[Movement CA]
+
+INSERT INTO [dbo].[Movement CA](SavingsAccountId, 
+							    TypeMovId, 
+								AccountStatementId,
+								Amount, 
+								NewBalance, 
+								[Description],
+								Visible, 
+								DateOfMov)
+VALUES(1, 1, 1, 100, 101, 'Restaurante El Arrecife', 0, '2000-11-11')
+
+/*opcion 1 NO FUNCIONA
+DECLARE @SearchWord NVARCHAR(100)  
+SET @SearchWord ='Restaurante'  
+SELECT MV.[Description]
+FROM [dbo].[Movement CA] MV
+WHERE CONTAINS([Description], @SearchWord);  
+*/
+
+--Opcion 2 funcionando
+DECLARE @SearchWord VARCHAR(100)
+SET @SearchWord ='%Retiro%'   --La palabra o frase a buscar tiene que ir entre '%' esto lo podemos arreglar en capa logica
+
+SELECT MV.[Description]
+FROM [dbo].[Movement CA] MV
+WHERE MV.[Description] LIKE @SearchWord
