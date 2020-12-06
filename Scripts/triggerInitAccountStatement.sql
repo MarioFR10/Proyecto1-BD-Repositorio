e03ee4e05284b479e7e330da7af579592eff1a4d
Irsacfac <irsacfac@gmail.com>
2020-12-05 19:24:46 -0600

    Update triggerInitAccountStatement.sql

--- Scripts/triggerInitAccountStatement.sql
+++ Scripts/triggerInitAccountStatement.sql
@@ -1,4 +1,4 @@
-CREATE TRIGGER [dbo].initAccountStatement ON [dbo].[SavingsAccount]
+ALTER TRIGGER [dbo].initAccountStatement ON [dbo].[SavingsAccount]
 AFTER INSERT
 --NOT FOR REPLICATION 
 AS
@@ -8,12 +8,18 @@ BEGIN
 										EndDate,
 										InitialBalance,
 										FinalBalance,
+										MinBalance,
+										ATMOps,
+										HumOps,
 										InsertAt,
 										InsertBy,
 										InsertIn)
 	SELECT i.Id,
 		   i.CreationDate,
 		   DATEADD( DAY, -1, DATEADD(MONTH, 1, i.CreationDate)),
+		   0,
+		   0,
+		   0,
 		   0,
 		   0,
 		   GETDATE(),
